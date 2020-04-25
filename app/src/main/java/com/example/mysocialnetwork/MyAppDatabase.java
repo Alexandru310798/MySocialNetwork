9af4a89886e561201be532c0c369548d95339a1a
Alexandru310798 <36679435+Alexandru310798@users.noreply.github.com>
2020-04-25 20:24:41 +0300

    Update MyAppDatabase.java

--- app/src/main/java/com/example/mysocialnetwork/MyAppDatabase.java
+++ app/src/main/java/com/example/mysocialnetwork/MyAppDatabase.java
@@ -2,7 +2,7 @@ package com.example.mysocialnetwork;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
-@Database(entities = {User.class},version = 1)
+@Database(entities = {User.class},version = 1, exportSchema = false)
 public abstract class MyAppDatabase extends RoomDatabase {
     public MyDao myDao;
 }
