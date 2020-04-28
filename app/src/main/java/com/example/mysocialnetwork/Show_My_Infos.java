bb0abeb29743c9c1670f51ca716931f3e8c3b67b
Alexandru310798 <36679435+Alexandru310798@users.noreply.github.com>
2020-04-28 18:35:04 +0300

    update landscape

--- app/src/main/java/com/example/mysocialnetwork/Show_My_Infos.java
+++ app/src/main/java/com/example/mysocialnetwork/Show_My_Infos.java
@@ -1,8 +1,26 @@
 package com.example.mysocialnetwork;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 import androidx.fragment.app.Fragment;
 
 public class Show_My_Infos extends Fragment {
     public String myName = "Nitulescu Alexandru", myEmail= "alexandrunitulescu@yahoo.com",myAge= "22";
+    TextView my_Infos;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.show_me, container, false);
+        my_Infos = view.findViewById(R.id.my_infos);
+        String my_Infos_String = "  I am : " + myName + "\nYou can find me at : " + myEmail + "\n I am " + myAge + " years old";
+        my_Infos.setText(my_Infos_String);
+        return view;
+    }
 
 }
